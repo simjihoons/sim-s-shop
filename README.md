fdd8b43e4a916feb6980f485e98cb21e8684ea6d
simjihoons <wlgnsgit@gmail.com>
2022-05-09 21:27:54 +0900

    기본 뼈대

--- README.md
+++
@@ -1,6 +0,0 @@
-# sim-s-shop
-
-node 10.16.0
-npm 8.5.0
-
-"express": "^4.18.1"
--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-//백엔드의 시작점
 const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => res.send("hello world!!!"));
+app.get("/", (req, res) => res.send("Helloasdasd"));
 
-app.listen(port, () => console.log(`${port}로 시작...`));
+app.listen(port, () => console.log(`${port}로 연결중...`));
