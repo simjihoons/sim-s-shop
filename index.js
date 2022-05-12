7403f67e2fbc0eb81280349d138e8ec13ac4c0e8
simjihoons <wlgnsgit@gmail.com>
2022-05-12 17:24:30 +0900

    비밀번호 암호화

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ app.get("/", (req, res) => res.send("Helloasdasd!!!!!!!!@@@@!"));
 app.post("/register", (req, res) => {
   const user = new User(req.body);
 
+  //password 암호화
   user.save((err, userInfo) => {
     if (err) return res.json({ success: false, err });
     return res.status(200).json({ success: true });
